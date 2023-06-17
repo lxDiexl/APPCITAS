30721ff496bd57736b8dc209442a8d47e5e650c8
lxDiexl <di.tasayco@gmail.com>
2023-06-17 03:12:07 -0500

    Update

--- resources/views/specialties/create.blade.php
+++ resources/views/specialties/create.blade.php
@@ -10,21 +10,7 @@
                             <h3 style="letter-spacing: 3px;">Nueva especialidad</h3>
 
                         </div>
-                        {{-- <div class="col-lg-6 col-5 my-auto text-end">
-                            <div class="dropdown float-lg-end pe-4">
-                                <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <i class="fa fa-ellipsis-v text-secondary"></i>
-                                </a>
-                                <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
-                                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Action</a></li>
-                                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Another action</a>
-                                    </li>
-                                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Something else
-                                            here</a></li>
-                                </ul>
-                            </div>
-                        </div> --}}
+
                         <div class="col-lg-6 col-5 my-auto text-end">
                             <a href="{{ url('/especialidades') }}" class="btn btn-sm btn-info"><i
                                     class="fas fa-arrow-alt-circle-left" style="font-size:15px;"></i> Regresar</a>
